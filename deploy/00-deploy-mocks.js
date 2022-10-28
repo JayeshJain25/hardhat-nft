bacf8aecf89415c65a41e660d875e2744b98cf75
jayesh jain <jayeshkjain6@gmail.com>
2022-10-28 10:35:52 +0530

    final commit

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,5 +1,6 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
+const { DECIMALS, INITIAL_PRICE } = require("../helper-hardhat-config")
 
 const BASE_FEE = ethers.utils.parseEther("0.25")
 const GAS_PRICE_LINK = 1e9
@@ -17,9 +18,20 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
             log: true,
             args: [BASE_FEE, GAS_PRICE_LINK],
         })
+
+        await deploy("MockV3Aggregator", {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_PRICE],
+        })
         log("Mocks Deployed!")
         log("-------------------------------")
+        log("You are deploying to a local network, you'll need a local network running to interact")
+        log(
+            "Please run `yarn hardhat console --network localhost` to interact with the deployed smart contracts!"
+        )
+        log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     }
 }
 
-module.exports.tags = ["all", "mocks"]
+module.exports.tags = ["all", "mocks", "main"]
